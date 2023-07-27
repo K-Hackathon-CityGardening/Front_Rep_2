87caec1499bc6eed9948abdcb44e84319568ac51
ohsung0722 <99doldol@gmail.com>
2023-07-27 21:39:06 +0900

    [ADD] 네이게이터 헤더 디자인 추가

--- components/BottomNavigator.jsx
+++ components/BottomNavigator.jsx
@@ -1,4 +1,4 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Image, Button, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../pages/HomePage.jsx';
@@ -9,14 +9,115 @@ import GardeningScreen from '../pages/GardeningPage.jsx';
 
 const Tab = createBottomTabNavigator();
 
+const styles = StyleSheet.create({
+  headerTitleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  tabContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+    borderTopColor: '#DDDDDD',
+    borderTopWidth: 1,
+  },
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tabLabel: {
+    fontSize: 12,
+  },
+  tabIcon: {
+    width: 26,
+    height: 26,
+  },
+  headerLogo: {
+    width: 60,
+    height: 60,
+  },
+  headerTitleText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+});
+
+function CustomHeaderTitle({title}) {
+  return (
+    <View style={styles.headerTitleContainer}>
+      <Image
+        style={styles.headerLogo}
+        source={require('../assets/Header_Icon.png')}
+      />
+      <Text style={styles.headerTitleText}>{title}</Text>
+    </View>
+  );
+}
+
 function BottomTabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Volunteer" component={VolunteerScreen} />
-      <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Gardening" component={GardeningScreen} />
-      <Tab.Screen name="Profile" component={MyPageScreen} />
+    <Tab.Navigator
+      ScreenOptions={{
+        style: styles.tabContainer,
+        showLabel: false,
+        showIcon: false,
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitle: () => <CustomHeaderTitle title="Home" />,
+          headerTitleStyle: {color: '#fff', fontWeight: 'normal'},
+          headerStyle: {
+            backgroundColor: '#77EBC1',
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Volunteer"
+        component={VolunteerScreen}
+        options={{
+          headerTitle: () => <CustomHeaderTitle title="Volunteer" />,
+          headerTitleStyle: {color: '#fff', fontWeight: 'normal'},
+          headerStyle: {
+            backgroundColor: '#77EBC1',
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          headerTitle: () => <CustomHeaderTitle title="Map" />,
+          headerTitleStyle: {color: '#fff', fontWeight: 'normal'},
+          headerStyle: {
+            backgroundColor: '#77EBC1',
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Gardening"
+        component={GardeningScreen}
+        options={{
+          headerTitle: () => <CustomHeaderTitle title="Gardening" />,
+          headerTitleStyle: {color: '#fff', fontWeight: 'normal'},
+          headerStyle: {
+            backgroundColor: '#77EBC1',
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={MyPageScreen}
+        options={{
+          headerTitle: () => <CustomHeaderTitle title="Profile" />,
+          headerTitleStyle: {color: '#fff', fontWeight: 'normal'},
+          headerStyle: {
+            backgroundColor: '#77EBC1',
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
